ca554ad3d2f4cf9697e2b13cdea0640a4d569cf0
PRATAP-KUMAR <pratap@fastmail.fm>
2022-08-24 11:40:34 +0530

    CREATE BASIC PAGE STRUCTURE

--- src/index.js
+++ src/index.js
@@ -1,12 +1,93 @@
 import './style.css';
+import data from './modules/data.js';
 
-function component() {
-  const element = document.createElement('div');
+const component = () => {
+  const box = document.createElement('div');
+  box.className = 'box';
 
-  element.innerText = 'Leaderboard';
-  element.className = 'hello';
+  const h2 = document.createElement('h2');
+  h2.innerText = 'Leaderboard';
 
-  return element;
-}
+  box.appendChild(h2);
+
+  const columnbox = document.createElement('div');
+  columnbox.className = 'columnbox';
+
+  box.appendChild(columnbox);
+
+  const column1 = document.createElement('div');
+  column1.className = 'column1';
+
+  columnbox.appendChild(column1);
+
+  const column2 = document.createElement('div');
+  column2.className = 'column2';
+
+  columnbox.appendChild(column2);
+
+  const row = document.createElement('div');
+  row.className = 'row';
+
+  column1.appendChild(row);
+
+  const container = document.createElement('div');
+  container.className = 'container';
+
+  column1.appendChild(container);
+
+  const recentscore = document.createElement('div');
+  recentscore.className = 'recentscore';
+  recentscore.innerText = 'Recent Scores';
+
+  row.appendChild(recentscore);
+
+  const refreshbutton = document.createElement('button');
+  refreshbutton.innerText = 'Refresh';
+  row.appendChild(refreshbutton);
+
+  const addscore = document.createElement('div');
+  addscore.className = 'addscore';
+  addscore.innerText = 'Add your score';
+
+  column2.appendChild(addscore);
+
+  const form1 = document.createElement('form');
+  form1.className = 'form1';
+  form1.setAttribute('method', 'post');
+
+  const FN = document.createElement('input');
+  FN.setAttribute('type', 'text');
+  FN.setAttribute('name', 'Name');
+  FN.setAttribute('placeholder', 'Your Name');
+
+  form1.appendChild(FN);
+
+  const form2 = document.createElement('form');
+  form2.className = 'form2';
+  form2.setAttribute('method', 'post');
+
+  const FS = document.createElement('input');
+  FS.setAttribute('type', 'text');
+  FS.setAttribute('name', 'score');
+  FS.setAttribute('placeholder', 'Your score');
+
+  form2.appendChild(FS);
+
+  const submit = document.createElement('button');
+  submit.innerText = 'Submit';
+
+  column2.appendChild(form1);
+  column2.appendChild(form2);
+  column2.appendChild(submit);
+
+  data.forEach((item) => {
+    const namescore = document.createElement('div');
+    namescore.className = 'namescore';
+    namescore.innerText = `${item.name}: ${item.score}`;
+    container.appendChild(namescore);
+  });
+
+  return box;
+};
 
 document.body.appendChild(component());
---
+++ src/modules/data.js
@@ -0,0 +1,36 @@
+const content = [
+  {
+    name: 'ABC',
+    score: 80,
+  },
+  {
+    name: 'DEF',
+    score: 90,
+  },
+  {
+    name: 'GHI',
+    score: 85,
+  },
+  {
+    name: 'KLM',
+    score: 90,
+  },
+  {
+    name: 'NOP',
+    score: 80,
+  },
+  {
+    name: 'QRS',
+    score: 75,
+  },
+  {
+    name: 'TUV',
+    score: 67,
+  },
+  {
+    name: 'WXY',
+    score: 90,
+  },
+];
+
+export default content;
